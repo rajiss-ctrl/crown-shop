252a3873638d00585ddfdff510edd88c6773481b
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2021-07-25 19:42:37 +0100

    homepage, directory and menu-item page commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import HomePage from './pages/homepage//homepage.component';
+import HomePage from './components/pages/homepage.component';
 import './App.css';
 
 
---
+++ src/components/pages/homepage.component.js
@@ -0,0 +1,12 @@
+
+import Directory from "../directory/directory.component";
+
+const HomePage = () => {
+    return ( 
+        <div className="homepage">
+           <Directory/>
+        </div>
+     );
+}
+ 
+export default HomePage;
