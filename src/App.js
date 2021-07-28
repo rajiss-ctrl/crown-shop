c7b171e888e0552ae1ed667c083930dbf6d97d69
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2021-07-28 07:46:15 +0100

    collection preview and shop componrents added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import './App.css';
+
 
 
 
@@ -12,7 +14,9 @@ import './App.css';
 function App() {
   return (
     <div>
+    
     <Router>
+        <Header/>
       <Switch>
             <Route exact path='/'>
               <HomePage/>
